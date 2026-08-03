1093769ecfbdb238d0d770416cbcc05e0dd6e21d
eg013ra1n <vilen.sharifov@gmail.com>
2026-08-04 00:07:30 +0300

    chore(frontend): delete ten orphan files (audit 2026-08-03)

--- src/components/export/index.ts
+++
@@ -1,7 +0,0 @@
-export { ExportTab } from './ExportTab';
-// Enhanced Export Summary components
-export { ExportSummary } from './ExportSummary';
-export { EquipmentHeader } from './EquipmentHeader';
-export { FilterGroupCard } from './FilterGroupCard';
-export { FolderStructurePreview } from './FolderStructurePreview';
-export { WarningsPanel } from './WarningsPanel';
